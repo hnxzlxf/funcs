708137ae7803d1092d413a514207c799118254ab
canton <yaogd1633@163.com>
2015-06-28 09:59:58 +0800

    first commit

---
+++ Yaogd-one/test/test/Test.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import org.apache.http.protocol.HttpContext;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	
+}
+
